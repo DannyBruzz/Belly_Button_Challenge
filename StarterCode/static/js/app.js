7cd1933000a577587ffaabec796007852709f79b
Danny <dannybruzz@hotmail.com>
2022-10-10 17:48:54 +0800

    progress

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,99 +1,72 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 d3.json(url).then(function (data) {
-    console.log(data);
-    let metaData = (data.metadata);
-    for (let i = 0; i < metaData.length; i++){
-        row = metaData[i];
-        participants.push(row);
+    let metaData = data.metadata;
+    let sample = 960;
+    let resultArray = metaData.filter(metaData => metaData.id == sample);
+    console.log(resultArray);
+    let resultAge = resultArray.map(resultArray => resultArray.age);
+    console.log(resultAge);
+    let resultBB= resultArray.map(resultArray => resultArray.bbtype);
+    console.log(resultBB);
+    let resultEthnic = resultArray.map(resultArray => resultArray.ethnicity);
+    console.log(resultEthnic);
+    let resultGender = resultArray.map(resultArray => resultArray.gender);
+    console.log(resultGender);
+    let resultLocation = resultArray.map(resultArray => resultArray.location);
+    console.log(resultLocation);
+    let resultWF = resultArray.map(resultArray => resultArray.wfreq);
+    console.log(resultWF);
 
-    }
-    console.log(participants)
-    let allBact = (data.samples);
-    for (let i = 0; i < allBact.length; i++){
-        row = allBact[i];
-        bacteria.push(row);
-    }
-    console.log(bacteria)
-    let allName = (data.names);
-    for (let i = 0; i < allName.length; i++){
-        row = allName[i];
-        nameID.push(row);
-    }
-    console.log(nameID);
+  
+    let samples = data.samples;
+    let sampleArray = samples.filter(samples => samples.id == sample);
+    console.log(sampleArray);
+    let samplesValues = sampleArray.map(sampleArray => sampleArray.sample_values);
+    console.log(samplesValues);
+    let samplesOTUID = sampleArray.map(sampleArray => sampleArray.otu_ids);
+    console.log(samplesOTUID);
+    let samplesOTUlabel = sampleArray.map(sampleArray => sampleArray.otu_labels);
+    console.log(samplesOTUlabel);
+  
 });
 
 
-let participants = []
-console.log(participants[2])
-let bacteria = []
-console.log(bacteria[2])
-let nameID = [] 
-console.log(nameID[2])
 
-d3.json(url).then(function (data) {
-    console.log(bacteria)
-    let allName = (data.names);
-    for (let i = 0; i < allName.length; i++){
-        row = allName[i];
-        nameID.push(row);
-        let j = Object.values(row)
-        if j = '254' {   
+
+function horizonPlot() {
+    let trace1 = {
+        x: samplesOTUID,
+        y: samplesValues,
+        text: samplesOTUlabel,
+        name: "Horizontal Chart",
+        type: "bar",
+        orientation: "h"
+    };
+    let plotData = [trace1];
+    let plotLayout = {
+        title: "A Plotly plot",
+        height: 600,
+        width: 800
+    };
+    Plotly.newPlot("bar", plotData, plotLayout);
+
+    let trace2 = {
+        x: samplesOTUID,
+        y: samplesValues,
+        mode: 'markers',
+        text: samplesOTUlabel,
+        marker: {
+          size: samplesValues,
+          color: samplesOTUID,
         }
-    }
-    console.log(nameID);
-    console.log(data);
-    let metaData = (data.metadata);
-    for (let i = 0; i < metaData.length; i++){
-        row = metaData[i];
-        participants.push(row);
-
-    }
-    console.log(participants)
-    let allBact = (data.samples);
-    for (let i = 0; i < allBact.length; i++){
-        row = allBact[i];
-        bacteria.push(row);
-    }
-});
-
-
-// function horizonPlot() {
-//     let trace1 = {
-//         x: otu_ids,
-//         y: sample_values,
-//         text: otu_labels,
-//         name: "Horizontal Chart",
-//         type: "bar",
-//         orientation: "h"
-//     };
-//     let data = [trace1];
-//     let layout = {
-//         title: "A Plotly plot",
-//         height: 600,
-//         width: 800
-//     };
-//     Plotly.newPlot("bar", data, layout);
-//     }
-
-
-// function bubblePlot() {
-//     let trace1 = {
-//         x: otu_ids,
-//         y: sample_values,
-//         mode: 'markers',
-//         text: otu_labels,
-//         marker: {
-//           size: sample_values,
-//           color: otu_ids,
-//         }
-//     };
-//     let data = [trace1];
-//     let layout = {
-//     title: 'Marker Size',
-//     showlegend: false,
-//     height: 600,
-//     width: 600
-//     };
-//     Plotly.newPlot('bubble', data, layout);
-// }
+    };
+    let plotData2 = [trace2];
+    let plotLayout2 = {
+    title: 'Marker Size',
+    showlegend: false,
+    height: 600,
+    width: 600
+    };
+    Plotly.newPlot('bubble', plotData2, plotLayout2);
+ }
